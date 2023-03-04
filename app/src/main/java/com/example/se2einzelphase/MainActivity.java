6bf1a6bce362f291f9c602321cf61eeb3daf68d3
svbluem <svbluem@edu.aau.at>
2023-03-04 18:31:12 +0100

    3. Commit: Created a method for the button "Abschicken", which did not work yet until I had made an own Class for the clientSocket. Manifext.xml required additional permission

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMATNR(View view) throws IOException, InterruptedException {
+        EditText matNR = findViewById(R.id.editTextMATNR);
+        TextView tv = findViewById(R.id.answerServer);
+        String mtrklNR = String.valueOf(matNR.getEditableText());
+
+        TCPClient tcpClient = new TCPClient(mtrklNR);
+
+        tcpClient.start();
+        tcpClient.join();
+
+        String result = tcpClient.getResult();
+
+        tv.setText(result);
+
+    }
+
 }
+
